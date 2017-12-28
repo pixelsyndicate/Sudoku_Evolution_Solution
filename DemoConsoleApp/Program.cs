80cdeb0c810135f14d862514a78117d57a5f1a43
Wil Dobson <wdobson@pixelsyndicate.com>
2017-12-28 08:22:27 -0600

    added timing check in console test

--- DemoConsoleApp/Program.cs
+++ DemoConsoleApp/Program.cs
@@ -9,16 +9,20 @@ namespace DemoConsoleApp
         private static IDisplayMatrix _disp;
         static void Main(string[] args)
         {
-
+            long startTime = DateTime.UtcNow.Ticks;
             _disp = new ConsoleDisplayMatrix();
             _evo = new EvolutionSolution();
+            ProblemLevels thisLevel = ProblemLevels.Difficult;
 
-            Problem problem = ProblemGenerator.GetProblem(ProblemLevels.Easy);
-
+            Problem problem = ProblemGenerator.GetProblem(thisLevel);
+            Console.WriteLine("Starting problem resolution for a " + thisLevel + " puzzle.");
+       
 
             _evo = new EvolutionSolution();
             int[][] result = _evo.SolveWithinExtinctions(problem.Rows, 15, 5000, 20).Result;
+
             var resultErrors = SudokuTool.Errors(result);
+            long stopTime = DateTime.UtcNow.Ticks;
 
             if (SudokuTool.Errors(result) == 0)
                 Console.WriteLine("Optimal solution found. \n");
@@ -29,6 +33,9 @@ namespace DemoConsoleApp
 
             _disp.SplashView(_evo.stats, problem.Rows, result);
 
+            Console.WriteLine();
+            Console.ForegroundColor = (ConsoleColor.Cyan);
+            Console.WriteLine("This run took: " + TimeSpan.FromTicks(stopTime - startTime).TotalSeconds.ToString("#.##") + "seconds");
 
             Console.ReadLine();
         }
